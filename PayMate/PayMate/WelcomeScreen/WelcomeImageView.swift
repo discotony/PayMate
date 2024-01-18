1da7f0254851548a335be58acc3c5563c15d668e
Antony Bluemel <46239175+discotony@users.noreply.github.com>
2024-01-18 14:07:22 -0800

    Add welcome screen animations

--- PayMate/PayMate/WelcomeScreen/WelcomeImageView.swift
+++ PayMate/PayMate/WelcomeScreen/WelcomeImageView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct WelcomeImageView: View {
     
-    @State private var rotationDegrees: Double = 360.0
+    @State private var personRotationDegrees: Double = 0.0
+    @State private var arrowRotationDegrees: Double = 4.0
+    @State private var arrowScale: Double = 1.0
+    @State private var coinRotationDegrees: Double = 360.0
     @State private var isMovingRight: Bool = true
     
     var body: some View {
@@ -19,16 +22,36 @@ struct WelcomeImageView: View {
                 Image(.welcomeBackground)
                     .customScaleResize(widthScale: 0.8)
                 
-                Image(.coin)
+                Image(.welcomePerson)
+                    .customScaleResize(widthScale: 0.8)
+                    .rotationEffect(.degrees(personRotationDegrees))
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                            personRotationDegrees = 3.0
+                        }
+                    }
+                
+                Image(.welcomeCoin)
                     .customScaleResize(widthScale: 0.08)
-                    .rotationEffect(.degrees(rotationDegrees))
+                    .rotationEffect(.degrees(coinRotationDegrees))
                     .offset(x: isMovingRight ? geometry.size.width * -0.08: geometry.size.width * -0.335, y: geometry.size.height * 0.355)
                     // Dynamically set offset values based on screen sizes
                 
                     .animation(Animation.easeInOut(duration: 8).repeatForever(autoreverses: true), value: isMovingRight)
                     .onAppear {
-                        rotationDegrees = 0
+                        coinRotationDegrees = 0
                         isMovingRight.toggle()
+                    }
+                
+                Image(.welcomeArrow)
+                    .customScaleResize(widthScale: 0.8)
+                    .rotationEffect(.degrees(arrowRotationDegrees))
+                    .scaleEffect(arrowScale)
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                            arrowRotationDegrees = 0
+                            arrowScale = 1.05
+                        }
                     }
             }
         }
